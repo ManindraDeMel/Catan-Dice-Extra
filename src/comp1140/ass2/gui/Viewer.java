a5a59da1fae604a5b5a0f1e4b5e99a7f003a4cc7
Arjun Raj <u7526852@anu.edu.au>
2022-09-17 09:18:50 +1000

    Added roads. Commit before code clean up

--- src/comp1140/ass2/gui/Viewer.java
+++ src/comp1140/ass2/gui/Viewer.java
@@ -15,6 +15,7 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 
 public class Viewer extends Application {
@@ -30,6 +31,7 @@ public class Viewer extends Application {
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     private final Group board = new Group();
+    private final Group structures = new Group();
 
 //    private static final double HEX_HEIGHT = 150;
 //    private static final double HEX_WIDTH = (int) (HEX_HEIGHT * Math.sqrt(3) / 2);
@@ -93,56 +95,6 @@ public class Viewer extends Application {
             }
         }
     }
-
-//    class Road extends Rectangle {
-//        double startX, startY;
-//
-//        Road(double startX, double startY) {
-//            this.startX = startX;
-//            this.startY = startY;
-//            this.setX(startX);
-//            this.setY(startY);
-//
-//            this.addHexagonPoints();
-//
-//            Color fillColour;
-//            Color strokeColour;
-//            fillColour = Color.DARKBLUE;
-//            strokeColour = Color.LIGHTBLUE;
-////            if (iceBlock == null) {
-////                fillColour = Color.DARKBLUE;
-////                strokeColour = Color.LIGHTBLUE;
-////            }
-////            else {
-////                strokeColour = Color.BLUE;
-////                if (iceBlock instanceof DraggableIceBlock)
-////                    fillColour = Color.ALICEBLUE;
-////                else
-////                    fillColour = SOLUTION_COLOURS[iceBlock.ice.getId() - 'A'];
-////            }
-//            this.setStroke(strokeColour);
-//            this.setStrokeWidth(2.0);
-//            this.setStrokeType(StrokeType.INSIDE);
-//            this.setFill(fillColour);
-//        }
-//
-//        /**
-//         * Add the points necessary for each hexagon in the game.
-//         */
-//        private void addRoadPoints() {
-//            double sideLength = VIEWER_HEIGHT * 3 / 5;
-//            double bearing = 0;
-//            for (int i = 0; i < 6; i++) {
-//                double x = Math.cos(Math.PI / 18 * bearing) * sideLength;
-//                double y = Math.sin(Math.PI / 18 * bearing) * sideLength;
-//                this.getPoints().add(x);
-//                this.getPoints().add(y);
-//                bearing += 6;
-//            }
-//        }
-//    }
-
-
 
     class KnightHead extends Circle{
         double startX, startY;
@@ -245,10 +197,81 @@ public class Viewer extends Application {
 //        }
 //    }
 
+    class Road extends Rectangle{
+        double height, width, startX, startY;
+        int id;
+
+        Road(double startX, double startY, boolean isBuilt, int id, char player){
+            this.startX = startX;
+            this.startY = startY;
+            this.id = id;
+
+            double sideLength = HEX_WIDTH * 3 / 5;
+            double width = HEX_WIDTH/8;
+            double height = width*2.5;
+
+            double centerX = startX-HEX_WIDTH/2 - width/2;
+            double centerY = startY - (sideLength - height)/2;
+
+//            switch (this.id){
+//                case 0 -> {
+//                    this.setX(centerX-HEX_WIDTH/2- width/2);
+//                    this.setY(centerY - (HEX_WIDTH)/2  + (sideLength - height)/2);
+//                    this.setWidth(width);
+//                    this.setHeight(height);
+//                }
+//                case 1 -> {
+//
+//                }
+//                case 3 -> {
+//                    this.setX(centerX + HEX_WIDTH/2 - width/2);
+//                    this.setY(centerY - (HEX_WIDTH)/2  + (sideLength - height)/2);
+//                    this.setWidth(width);
+//                    this.setHeight(height);
+//                }
+//            }
+
+            this.setX(centerX);
+            this.setY(centerY);
+            this.setWidth(width);
+            this.setHeight(height);
+
+            Rotate rotate = new Rotate();
+            rotate.setPivotX(startX);
+            rotate.setPivotY(startY);
+            this.getTransforms().addAll(rotate);
+            rotate.setAngle(30*id);
+            this.getTransforms().add(rotate);
+
+            Color fillColour;
+            Color strokeColour;
+            if (isBuilt == false) {
+                fillColour = Color.DARKGRAY;
+                strokeColour = Color.LIGHTGRAY;
+            }
+            else {
+                strokeColour = Color.BLUE;
+                if (player == 'w')
+                    fillColour = playersColour[0];
+                else
+                    fillColour = playersColour[1];
+            }
+
+            this.setStroke(strokeColour);
+            this.setStrokeWidth(2.0);
+            this.setStrokeType(StrokeType.INSIDE);
+            this.setFill(fillColour);
+        }
+    }
+
     private void makeBoard() {
         this.board.setLayoutX(BOARD_X + MARGIN_X);
         this.board.setLayoutY(BOARD_Y + MARGIN_Y);
         this.board.getChildren().clear();
+
+        this.structures.setLayoutX(BOARD_X + MARGIN_X);
+        this.structures.setLayoutY(BOARD_Y + MARGIN_Y);
+        this.structures.getChildren().clear();
 
         for (int y = 0; y < 5; y++) {
             for (int x= 0; x < 5; x++) {
@@ -304,18 +327,119 @@ public class Viewer extends Application {
 //                rectangle.setStroke(Color.valueOf("0x000000"));
 //                this.board.getChildren().add(rectangle);
 
+                // MAKING ROAD
+
+                // left
+//                double sideLength = HEX_WIDTH * 3 / 5;
+//                Rectangle rectangle = new Rectangle();
+//                double width = HEX_WIDTH/8;
+//                double height = width*2.5;
+//                //Setting the properties of the rectangle
+////                rectangle.setX(centerX-HEX_WIDTH/2- width/2);
+////                rectangle.setY(centerY - (HEX_WIDTH)/2  + (sideLength - height)/2);
+//
+//                rectangle.setX(startX-HEX_WIDTH/2 - width/2);
+//                rectangle.setY(startY - (sideLength - height)/2);
+//                rectangle.setWidth(width);
+//                rectangle.setHeight(height);
+////                rectangle.setRotate(180);
+//
+//                rectangle.setFill(Color.GREEN);
+//                rectangle.setStrokeWidth(2);
+//                rectangle.setStrokeMiterLimit(10);
+//                rectangle.setStrokeType(StrokeType.INSIDE);
+//                rectangle.setStroke(Color.valueOf("0x000000"));
+//                this.board.getChildren().add(rectangle);
+
+                // right
+//                rectangle = new Rectangle();
+//                //Setting the properties of the rectangle
+//                rectangle.setX(centerX + HEX_WIDTH/2 - width/2);
+//                rectangle.setY(centerY - (HEX_WIDTH)/2  + (sideLength - height)/2);
+//                rectangle.setWidth(width);
+//                rectangle.setHeight(height);
+////                rectangle.setRotate(90);
+//
+//                rectangle.setFill(Color.GREEN);
+//                rectangle.setStrokeWidth(2);
+//                rectangle.setStrokeMiterLimit(10);
+//                rectangle.setStrokeType(StrokeType.INSIDE);
+//                rectangle.setStroke(Color.valueOf("0x000000"));
+//                this.board.getChildren().add(rectangle);
+
+                // top left
+//                rectangle = new Rectangle();
+//                //Setting the properties of the rectangle
+//                rectangle.setX(startX-HEX_WIDTH/2 - width/2);
+//                rectangle.setY(startY - (sideLength - height)/2);
+//                rectangle.setWidth(width);
+//                rectangle.setHeight(height);
+////                rectangle.setRotate(90);
+//
+//                Rotate rotate = new Rotate();
+//                rotate.setPivotX(startX);
+//                rotate.setPivotY(startY);
+//                rectangle.getTransforms().addAll(rotate);
+//                rotate.setAngle(30);
+//                rectangle.getTransforms().add(rotate);
+//
+//                rectangle.setFill(Color.GREEN);
+//                rectangle.setStrokeWidth(2);
+//                rectangle.setStrokeMiterLimit(10);
+//                rectangle.setStrokeType(StrokeType.INSIDE);
+//                rectangle.setStroke(Color.valueOf("0x000000"));
+//                this.board.getChildren().add(rectangle);
+//
+//                // top right
+//                rectangle = new Rectangle();
+//                //Setting the properties of the rectangle
+//                rectangle.setX(startX-HEX_WIDTH/2 - width/2);
+//                rectangle.setY(startY - (sideLength - height)/2);
+//                rectangle.setWidth(width);
+//                rectangle.setHeight(height);
+////                rectangle.setRotate(90);
+//
+//                rotate = new Rotate();
+//                rotate.setPivotX(startX);
+//                rotate.setPivotY(startY);
+//                rectangle.getTransforms().addAll(rotate);
+//                rotate.setAngle(60);
+//                rectangle.getTransforms().add(rotate);
+//
+//                rectangle.setFill(Color.GREEN);
+//                rectangle.setStrokeWidth(2);
+//                rectangle.setStrokeMiterLimit(10);
+//                rectangle.setStrokeType(StrokeType.INSIDE);
+//                rectangle.setStroke(Color.valueOf("0x000000"));
+//                this.board.getChildren().add(rectangle);
+
 
 //                KnightHead knightHead = new KnightHead(startX, startY, false, 'w');
                 Knight knight;
+                Road road = new Road(startX, startY, false, x,'w');
                 switch ((x+y)%3){
+
                     case 0 -> knight = new Knight(startX, startY, false, 'w');
-                    case 1 -> knight = new Knight(startX, startY, true, 'w');
-                    case 2 -> knight = new Knight(startX, startY, true, 'x');
-                    default -> knight = new Knight(startX, startY, false, 'w');
+
+                    case 1 -> {
+                        knight = new Knight(startX, startY, true, 'w');
+                        road = new Road(startX, startY, true, x,'w');
+                    }
+
+                    case 2 -> {
+                        knight = new Knight(startX, startY, true, 'x');
+                        road = new Road(startX, startY, true, x,'x');
+                    }
+
+                    default -> {
+                        knight = new Knight(startX, startY, false, 'w');
+                        road = new Road(startX, startY, false, x,'w');
+                    }
                 }
 //                Knight knight = new Knight(startX, startY, false, 'w');
-                this.board.getChildren().add(knight.knightBody);
-                this.board.getChildren().add(knight.knightHead);
+                this.structures.getChildren().add(knight.knightBody);
+                this.structures.getChildren().add(knight.knightHead);
+                this.structures.getChildren().add(road);
             }
         }
         this.board.toBack();
@@ -358,6 +482,7 @@ public class Viewer extends Application {
 
         root.getChildren().add(controls);
         this.root.getChildren().add(this.board);
+        this.root.getChildren().add(this.structures);
 
         this.makeBoard();
         makeControls();
