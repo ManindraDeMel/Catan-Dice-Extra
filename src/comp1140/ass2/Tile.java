733b297b256769ec5bd0429a16cb3e586a7d7a55
Manindra De Mel <u7156805@anu.edu.au>
2022-09-19 14:59:16 +1000

    Revert "Merge branch 'stephen' into 'main'"
    This reverts merge request !6

--- src/comp1140/ass2/Tile.java
+++ src/comp1140/ass2/Tile.java
@@ -3,12 +3,8 @@ package comp1140.ass2;
 public class Tile extends GamePiece{
     public Boolean used;
     public Resource resourceType;
-    public Coordinate[] location;
-    public int tileIndex;
-    public Tile(Player player, Coordinate[] coords, int index) {
+    public Coordinate[] location = new Coordinate[6];
+    Tile(Player player) {
         super(player);
-        this.location = coords;
-        this.tileIndex=index;
     }
-
 }
