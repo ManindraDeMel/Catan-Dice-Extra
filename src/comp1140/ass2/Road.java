7f28eb70e71b7ef233b8bae8e1b7b05785f047c1
Stephen Burg <u7146285@anu.edu.au>
2022-10-05 14:21:26 +1100

    Stephen

--- src/comp1140/ass2/Road.java
+++ src/comp1140/ass2/Road.java
@@ -5,7 +5,14 @@ import java.util.Arrays;
 public class Road extends GamePiece{
     Coordinate coord1;
     Coordinate coord2;
-
+    /**
+     * Road:
+     * Has fields:
+     * player - its owner if built
+     * coord1 - the coordinate of the start (smaller indexed end) of the road
+     * coord2 - the coordinate of the end (larger indexed end) of the road
+     * Authored by Stephen Burg - u7146285 based on ideas created collaboratively with the group
+     */
     public Road(Player player, Coordinate coord1, Coordinate coord2) {
         super(player);
         this.coord1 = coord1;
--- src/comp1140/ass2/Settlement.java
+++ src/comp1140/ass2/Settlement.java
@@ -9,11 +9,23 @@ public class Settlement extends GamePiece{
     public Boolean isBuilt;
     public Coordinate coord;
     public int intersectionIndex;
-
+    /**
+     * Settlement:
+     * Has fields:
+     * player - its owner if built
+     * coord - its coordinate on the grid
+     * cityable - whether it is upgradeable into a city
+     * isBuilt - whether it is built, always instantiated false (redundant maybe as if it has an owner it is built)
+     * isCity - whether it is a city, always instantiated false
+     * index - its single integer index in the position sytstem provided
+     * Authored by Stephen Burg - u7146285 based on ideas created collaboratively with the group
+     */
     public Settlement(Player player, Coordinate coord, Boolean cityable, int index) {
         super(player);
         this.coord = coord;
         this.isCityable=cityable;
+        this.isBuilt = false;
+        this.isCity = false;
         this.intersectionIndex =index;
     }
 
